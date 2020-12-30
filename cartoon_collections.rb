36f6bd395a164649f063dd86dc48c374366499de
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-30 00:06:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,7 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   
-  new_arr = []
-  index = 0
+  array.map do
 end
 
 def summon_captain_planet(planeteer_calls)
