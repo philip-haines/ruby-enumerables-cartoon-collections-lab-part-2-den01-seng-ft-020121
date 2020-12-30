94dc0ae5d7b7144c9946ef1719c9fd08b35e05b8
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-30 00:20:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
   
-  if planeteer_calls.include?(String.length > 4)
+  if planeteer_calls.find_all
     true
   end
 end
